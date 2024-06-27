d2c649768bf474a2eb740a36db00788e98433613
unknown <blackhorse0215@gmail.com>
2024-06-27 04:11:04 -0700

    sdfsf

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: 'src/app.jsx',
+      input: 'src/main.jsx',
       output: {
         entryFileNames: 'main.js',
         assetFileNames: 'tailwind.css',
